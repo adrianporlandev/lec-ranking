3409725f8debcb7004ca479b71a4b40f4388c46f
Adrian Porlan <adriporlanlopez11@gmail.com>
2024-08-29 00:18:04 +0200

    add vercel ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import icon from "astro-icon";
-
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/adapter-vercel";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), icon(), tailwind()],
   output: 'server', // o 'hybrid' para SSR
+  adapters: [vercel()],
 });
