12ac95c1c69c1c55721dddad653432a1fd83e9d1
Adrian Porlan <adriporlanlopez11@gmail.com>
2024-08-28 20:36:05 +0200

    clean the proyect

--- src/importImages.js
+++
@@ -1,8 +0,0 @@
-// src/importImages.js
-export const getRoleImage = (role) => {
-  return `https://res.cloudinary.com/dlfgycfb8/image/upload/v1724868748/lol-project/pos/${role}.png`;
-};
-
-export const getTierImage = (tier) => {
-  return `https://res.cloudinary.com/dlfgycfb8/image/upload/v1724868748/lol-project/elo/Rank%3D${tier}.png`;
-};
