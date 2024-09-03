2dc3c7a90b2867049663e31c257ab72eaf9fdda3
Adrian Porlan <adriporlanlopez11@gmail.com>
2024-09-03 15:45:32 +0200

    refactor app.py

---
+++ players.py
@@ -0,0 +1,71 @@
+players = [
+    # Mad Lion
+    {"name": "Lamine Yamal", "tag": "Annie", "realname": "Myrwn", "team": "MDK", "role": "top"},
+    {"name": "komanche uchiha", "tag": "elite", "realname": "Elyoya", "team": "MDK", "role": "jungle"},
+    {"name": "Mametchi", "tag": "tukan", "realname": "Fresskowy", "team": "MDK", "role": "mid"},
+    {"name": "tukaan", "tag": "tukan", "realname": "Supa", "team": "MDK", "role": "adc"},
+    {"name": "alvarooo", "tag": "EUW", "realname": "Alvaro", "team": "MDK", "role": "support"},
+    
+    # G2
+    { 'name': 'TheShackledOne', 'tag': '001', 'realname': 'Broken Blade', 'team': 'G2', 'role': 'top' },
+    { 'name': 'TheShackledOne', 'tag': '002', 'realname': 'Yike', 'team': 'G2', 'role': 'jungle' },
+    { 'name': 'TheShackledOne', 'tag': '003', 'realname': 'Caps', 'team': 'G2', 'role': 'mid' },
+    { 'name': 'TheShackledOne', 'tag': '004', 'realname': 'Hans Sama', 'team': 'G2', 'role': 'adc' },
+    { 'name': 'TheShackledOne', 'tag': '005', 'realname': 'Mikyx', 'team': 'G2', 'role': 'support' },
+    
+    # Fnatic
+    { 'name': 'papi oscar', 'tag': 'GOAT', 'realname': 'Oscarinin', 'team': 'FNC', 'role': 'top' },
+    { 'name': 'Razørk Activoo', 'tag': 'razzz', 'realname': 'Razork', 'team': 'FNC', 'role': 'jungle' },
+    { 'name': 'Wesley Warren Jr', 'tag': 'EUW', 'realname': 'Humanoid', 'team': 'FNC', 'role': 'mid' },
+    { 'name': 'Noah7', 'tag': 'Euw2', 'realname': 'Noah', 'team': 'FNC', 'role': 'adc' },
+    { 'name': 'qrds', 'tag': '211', 'realname': 'Jun', 'team': 'FNC', 'role': 'support' },
+    
+    # Team BDS
+    { 'name': 'Nothing much', 'tag': 'EUW', 'realname': 'Adam', 'team': 'BDS', 'role': 'top' },
+    { 'name': 'sorry for ping', 'tag': '1305', 'realname': 'Sheo', 'team': 'BDS', 'role': 'jungle' },
+    { 'name': 'BDSnuc', 'tag': 'EUW', 'realname': 'Nuc', 'team': 'BDS', 'role': 'mid' },
+    { 'name': 'happy game', 'tag': '713', 'realname': 'Ice', 'team': 'BDS', 'role': 'adc' },
+    { 'name': 'school phobia', 'tag': 'EUW', 'realname': 'Labrov', 'team': 'BDS', 'role': 'support' },
+    
+    # SK Gaming
+    { 'name': 'yuqi', 'tag': 'golem', 'realname': 'Irrelevant', 'team': 'SK', 'role': 'top' },
+    { 'name': '786786786', 'tag': 'LEC1', 'realname': 'Isma', 'team': 'SK', 'role': 'jungle' },
+    { 'name': 'CHAUVESQY', 'tag': 'LEC', 'realname': 'Nisqy', 'team': 'SK', 'role': 'mid' },
+    { 'name': 'Rahel', 'tag': '213', 'realname': 'Rahel', 'team': 'SK', 'role': 'adc' },
+    { 'name': 'Luon', 'tag': '0127', 'realname': 'Irrelevant', 'team': 'SK', 'role': 'support' },
+    
+    # GiantsX
+    { 'name': 'TH3', 'tag': 'GX1', 'realname': 'Th3Antonio', 'team': 'GX', 'role': 'top' },
+    { 'name': 'Juhana', 'tag': 'GXP', 'realname': 'Juhan', 'team': 'GX', 'role': 'jungle' },
+    { 'name': 'Ana de Armas', 'tag': 'SMRAD', 'realname': 'Jackies', 'team': 'GX', 'role': 'mid' },
+    { 'name': 'LeonardoDiCassio', 'tag': 'EUW', 'realname': 'Patrik', 'team': 'GX', 'role': 'adc' },
+    { 'name': 'P5MoSZ1jatp3JQYy', 'tag': 'EUW', 'realname': 'Ignar', 'team': 'GX', 'role': 'support' },
+    
+    # Karmine Corp
+    { 'name': 'EJRKLJ0', 'tag': '0000', 'realname': 'Canna', 'team': 'KC', 'role': 'top' },
+    { 'name': 'C1oser', 'tag': 'EUW', 'realname': 'Closer', 'team': 'KC', 'role': 'jungle' },
+    { 'name': 'J1HUIV', 'tag': '000', 'realname': 'Vladi', 'team': 'KC', 'role': 'mid' },
+    { 'name': 'Upset', 'tag': 'asdff', 'realname': 'Upset', 'team': 'KC', 'role': 'adc' },
+    { 'name': 'Targamas', 'tag': '5555', 'realname': 'Targamas', 'team': 'KC', 'role': 'support' },
+    
+    # Rogue
+    { 'name': 'Finn', 'tag': '0306', 'realname': 'Finn', 'team': 'RGE', 'role': 'top' },
+    { 'name': 'goodjob', 'tag': 'nerd', 'realname': 'Markoon', 'team': 'RGE', 'role': 'jungle' },
+    { 'name': 'george kittle', 'tag': 'EUW', 'realname': 'Larssen', 'team': 'RGE', 'role': 'mid' },
+    { 'name': 'Kite Machine', 'tag': 'EUW', 'realname': 'Comp', 'team': 'RGE', 'role': 'adc' },
+    { 'name': 'Walid Georgey', 'tag': 'EUW', 'realname': 'Zoelys', 'team': 'RGE', 'role': 'support' },
+    
+    # Team Heretics
+    { 'name': 'Wunderwear', 'tag': 'GOAT', 'realname': 'Wunder', 'team': 'TH', 'role': 'top' },
+    { 'name': 'PlaceHolderJG', 'tag': 'HRT', 'realname': 'Jankos', 'team': 'TH', 'role': 'jungle' },
+    { 'name': 'xwcyw', 'tag': 'soul', 'realname': 'Zwyroo', 'team': 'TH', 'role': 'mid' },
+    { 'name': 'Flakkardo', 'tag': 'METIN', 'realname': 'Flakked', 'team': 'TH', 'role': 'adc' },
+    { 'name': 'CEXMONSTER', 'tag': 'CEX69', 'realname': 'Trymbi', 'team': 'TH', 'role': 'support' },
+    
+    # Team Vitality
+    { 'name': 'is it just me', 'tag': 'EUW', 'realname': 'Photon', 'team': 'VIT', 'role': 'top' },
+    { 'name': 'VIT top boy', 'tag': 'lync1', 'realname': 'Lyncas', 'team': 'VIT', 'role': 'jungle' },
+    { 'name': 'I will win LEC', 'tag': '4321', 'realname': 'Vetheo', 'team': 'VIT', 'role': 'mid' },
+    { 'name': 'G2 Humanoid', 'tag': 'mis', 'realname': 'Carzzy', 'team': 'VIT', 'role': 'adc' },
+    { 'name': 'Peshon', 'tag': 'EUW', 'realname': 'Hylissang', 'team': 'VIT', 'role': 'support' }
+]
